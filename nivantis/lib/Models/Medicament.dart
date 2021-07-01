ff04661405298bf7e29e748ac951a47a05fcb0d5
v.berger <vincent.berger@epsi.fr>
2021-07-01 15:31:48 +0200

    Ajout modèle médicament

---
+++ nivantis/lib/Models/Medicament.dart
@@ -0,0 +1,9 @@
+class Medicament {
+  String? id;
+  String? name;
+  double? prixAchatBrut;
+  double? prixAchatnet;
+  double? tauxDeRemise;
+  double? prixVenteNet;
+  double? coefficientMultiplicateur;
+}
